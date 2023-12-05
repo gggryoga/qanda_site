cfb608c8140eb4de07459c348ee7c0c206f8e6ef
大関諒真 <ryoma@oozekiryoushinnoMacBook-Air.local>
2023-12-05 11:05:20 +0900

    fast commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,53 @@
+class QuestionsController < ApplicationController
+    #質問一覧表示
+    def index
+        @questions = Question.all
+    end
+
+    #質問詳細表示
+    def show
+        @question = Question.find(params[:id])
+        
+    end
+
+    #質問作成
+    def new
+        @question = Question.new
+    end
+
+    #質問登録
+    def create
+        @question = Question.new(question_params)
+        if @question.save
+            redirect_to @question
+        else
+            render 'new', status: :unprocessable_entity
+        end
+    end
+
+    #質問編集
+    def edit
+        @question = Question.find(params[:id])
+    end
+
+    #質問更新
+    def update
+        @question = Question.find(params[:id])
+        if @question.update(question_params)
+            redirect_to @question
+        else
+            render 'edit', status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @question = Question.find(params[:id])
+        @question.destroy
+        redirect_to questions_path
+    end
+
+    private
+    def question_params
+        params.require(:question).permit(:title, :name, :content)
+    end
+end
