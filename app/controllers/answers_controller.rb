861a80aaa0b4b05aeb76c11c6b7656aaecdd048c
大関諒真 <ryoma@oozekiryoushinnoMacBook-Air.local>
2023-12-07 17:04:02 +0900

    make answer

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,15 @@
+class AnswersController < ApplicationController
+    #回答登録
+    def create
+        @question = Question.find(params[:question_id])
+        @answer = @question.answers.create(answer_params)
+        redirect_to question_path(@question)
+    end
+
+    def destroy
+        @question = Question.find(params[:question_id])
+        @answer = @question.answers.find(params[:id])
+        @answer.destroy
+        redirect_to question_path(@question)
+    end
+end
